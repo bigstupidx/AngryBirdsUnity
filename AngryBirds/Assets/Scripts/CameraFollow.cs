c844fcc11d55f5727013c61f7b09a52651dbe144
Phan Tuệ Anh <ptahcmus@gmail.com>
2017-10-18 00:50:46 +0700

    cài đặt camera di chuyển lúc bắt đầu màn chơi

--- AngryBirds/Assets/Scripts/CameraFollow.cs
+++ AngryBirds/Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,7 @@ public class CameraFollow : MonoBehaviour {
 
     public Transform target;
     public float smoothing;
+    public float moveSpeed;
     Vector3 offset;
     float lowY;
     float leftX;
@@ -13,32 +14,52 @@ public class CameraFollow : MonoBehaviour {
     public float highY;
     public float rightX;
 
+    private bool isStarted;
+
     // Use this for initialization
     void Start()
     {
-        offset = transform.position - target.position;
-        lowY = transform.position.y;
-        leftX = transform.position.x;
+        isStarted = false;
+
+        
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (target)
+
+        if(!isStarted)
         {
-            if (!target.GetComponent<BallController>().getPressed())
+            transform.position -= Vector3.right * moveSpeed * Time.deltaTime;
+            if(transform.position.x <= 0.0f)
             {
-                Vector3 targetCamPos = target.position + offset;
-                transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+                transform.position = new Vector3(0.0f, transform.position.y, transform.position.z);
+                offset = transform.position - target.position;
+                lowY = transform.position.y;
+                leftX = transform.position.x;
+                isStarted = true;
             }
         }
-        if (transform.position.y < lowY)
-            transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
-        if (transform.position.y > highY)
-            transform.position = new Vector3(transform.position.x, highY, transform.position.z);
-        if (transform.position.x < leftX)
-            transform.position = new Vector3(leftX, transform.position.y, transform.position.z);
-        if (transform.position.x > rightX)
-            transform.position = new Vector3(rightX, transform.position.y, transform.position.z);
+
+        if (isStarted)
+        {
+            if (target)
+            {
+                if (!target.GetComponent<BallController>().getPressed())
+                {
+                    Vector3 targetCamPos = target.position + offset;
+                    transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+                }
+            }
+            if (transform.position.y < lowY)
+                transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
+            if (transform.position.y > highY)
+                transform.position = new Vector3(transform.position.x, highY, transform.position.z);
+            if (transform.position.x < leftX)
+                transform.position = new Vector3(leftX, transform.position.y, transform.position.z);
+            if (transform.position.x > rightX)
+                transform.position = new Vector3(rightX, transform.position.y, transform.position.z);
+        }
     }
 }
