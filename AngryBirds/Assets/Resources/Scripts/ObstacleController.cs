3bd9fe828838615479860b9d89b14abdeb1fc15f
Phan Tuệ Anh <ptahcmus@gmail.com>
2017-10-20 22:33:12 +0700

    thêm hiệu ứng mảnh vỡ khi gỗ bị phá hủy

--- AngryBirds/Assets/Resources/Scripts/ObstacleController.cs
+++ AngryBirds/Assets/Resources/Scripts/ObstacleController.cs
@@ -6,11 +6,12 @@ public class ObstacleController : MonoBehaviour {
 
     public Animator myAnim;
     public float health;
+    public bool isDead;
 
 	// Use this for initialization
 	void Start () 
     {
-		
+        isDead = false;
 	}
 
     void OnCollisionEnter2D(Collision2D otherColl)
@@ -18,6 +19,12 @@ public class ObstacleController : MonoBehaviour {
         if (otherColl.relativeVelocity.magnitude > health)
         {
             myAnim.SetBool("isDead", true);
+            if (!isDead)
+            {
+                GameObject dustPrefab = (GameObject)Resources.Load("Prefabs/Effects/wood dust PS", typeof(GameObject));
+                Instantiate(dustPrefab, transform.position, dustPrefab.transform.rotation);
+            }
+            isDead = true;
             Invoke("makeDead", 1.0f);
         }
     }
