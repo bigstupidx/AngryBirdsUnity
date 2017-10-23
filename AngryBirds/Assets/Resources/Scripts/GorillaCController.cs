bcf8884a4fd39516d265267176977cdedeec3905
Phan Tuệ Anh <ptahcmus@gmail.com>
2017-10-23 22:15:54 +0700

    thêm nhân vật gorilla mới

---
+++ AngryBirds/Assets/Resources/Scripts/GorillaCController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GorillaCController : MonoBehaviour {
+
+    public float health;
+    public Animator myAnim;
+
+    private bool isDead;
+
+    void Start()
+    {
+        isDead = false;
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D otherColl)
+    {
+        if (otherColl.relativeVelocity.magnitude > health)
+        {
+            isDead = true;
+            myAnim.SetBool("isDead", true);
+            Invoke("makeDead", 5.0f);
+        }
+    }
+
+
+    void makeDead()
+    {
+        Destroy(gameObject);
+    }
+	
+}
