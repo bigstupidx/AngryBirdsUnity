808a27a6391674d5e737c636aa23b329d7b03ae1
Phan Tuệ Anh <ptahcmus@gmail.com>
2017-10-25 21:22:49 +0700

    thêm animation run cho nhân vật panda

--- AngryBirds/Assets/Resources/Scripts/PlayersManager.cs
+++ AngryBirds/Assets/Resources/Scripts/PlayersManager.cs
@@ -14,7 +14,7 @@ public class PlayersManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-        strPlayer = "mouse";
+        strPlayer = "panda";
         numPlayers = 3;
         currentPlayer = 0;
         preparePlayers();
