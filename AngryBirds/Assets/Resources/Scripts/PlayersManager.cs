4326fe815199299785399fca0d45a4a00c73b854
Phan Tuệ Anh <ptahcmus@gmail.com>
2017-10-30 00:14:03 +0700

    cài đặt nhân vật dog

--- AngryBirds/Assets/Resources/Scripts/PlayersManager.cs
+++ AngryBirds/Assets/Resources/Scripts/PlayersManager.cs
@@ -14,7 +14,7 @@ public class PlayersManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-        strPlayer = "deer";
+        strPlayer = "dog";
         numPlayers = 3;
         currentPlayer = 0;
         preparePlayers();
