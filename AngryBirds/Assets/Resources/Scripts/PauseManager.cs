f3a43bfaccab67167d8d36e6163334c5c0f36307
Phan Tuệ Anh <ptahcmus@gmail.com>
2017-10-31 23:52:54 +0700

    cài đặt hệ thống pause

---
+++ AngryBirds/Assets/Resources/Scripts/PauseManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour {
+
+    public GameObject pausePanel;
+
+    public Image imgPause;
+    public Image imgMusic;
+    public Image imgSFX;
+    public Image imgResume;
+    public Image imgRestart;
+    public Image imgExit;
+
+    private bool isMusicOn;
+    private bool isSFXOn;
+
+	// Use this for initialization
+	void Start () 
+    {
+        isMusicOn = true;
+        isSFXOn = true;
+	}
+
+    public void doPause(bool pause)
+    {
+        if (pause)
+        {
+            imgPause.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Pressed/btn Pause pressed");
+            pausePanel.SetActive(true);
+            Invoke("stopTime", 1.0f);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void doResume(bool resume)
+    {
+        if (resume)
+        {
+            imgResume.color = new Color(1.0f, 1.0f, 1.0f, 0.55f);
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+            imgPause.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn Pause");
+            imgResume.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            pausePanel.GetComponent<Animator>().SetBool("isPausing", false);
+            Invoke("HidePausePanel", 1.0f);
+        }
+    }
+
+    void stopTime()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    void HidePausePanel()
+    {
+        pausePanel.SetActive(false);
+    }
+
+	public void OnButtonMusicPressed(bool pressed)
+    {
+        if(pressed)
+        {
+            if(isMusicOn)
+                imgMusic.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn music off");
+            else
+                imgMusic.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn music on");
+
+            isMusicOn = !isMusicOn;
+        }
+        else
+        {
+
+        }
+    }
+
+    public void OnButtonSFXPressed(bool pressed)
+    {
+        if (pressed)
+        {
+            if (isSFXOn)
+                imgSFX.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn sound effect off");
+            else
+                imgSFX.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn sound effect on");
+
+            isSFXOn = !isSFXOn;
+        }
+        else
+        {
+
+        }
+    }
+
+    
+}
