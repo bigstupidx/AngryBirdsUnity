b0b71f0b54bd785a13ed0597913c59b9e6f6b793
Phan Tuệ Anh <ptahcmus@gmail.com>
2017-11-04 00:38:07 +0700

    update ui menu + thêm logo intro

---
+++ AngryBirds/Assets/Resources/Scripts/MenuManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    public Image imgPlay;
+    public Image imgGame;
+    public Image imgRate;
+    public Image imgSetting;
+    public Image imgAboutus;
+
+    public GameObject levelSelectPanel;
+    public Image imgLeft;
+    public Image imgRight;
+    public Image imgClose;
+
+    public GameObject comingsoonPanel;
+    public Image imgOK;
+
+
+    private bool isAnyButtonPressed;
+
+    // Use this for initialization
+    void Start ()
+    {
+        isAnyButtonPressed = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnButtonPlayPressed(bool pressed)
+    {
+        if(pressed)
+        {
+            imgPlay.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Pressed/btn Next pressed");
+            levelSelectPanel.SetActive(true);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void OnButtonGamePressed(bool pressed)
+    {
+        if (pressed)
+        {
+            imgGame.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Pressed/btn Game pressed");
+            comingsoonPanel.SetActive(true);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void OnButtonRatePressed(bool pressed)
+    {
+        if (pressed)
+        {
+            imgRate.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Pressed/btn Rate pressed");
+            comingsoonPanel.SetActive(true);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void OnButtonClosePressed(bool pressed)
+    {
+        if (pressed)
+        {
+            imgClose.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Pressed/btn Close pressed");           
+        }
+        else
+        {
+            imgPlay.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn Next");
+            imgClose.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn Close");
+            levelSelectPanel.GetComponent<Animator>().SetBool("isClosed", true);
+            Invoke("hideLevelSelectPanel", 1.75f);
+        }
+    }
+
+    public void OnButtonOKPressed(bool pressed)
+    {
+        if (pressed)
+        {
+            imgOK.color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+        }
+        else
+        {
+            imgGame.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn Game");
+            imgRate.sprite = Resources.Load<Sprite>("Graphic/UI/Buttons/Normal/btn Rate");
+            imgOK.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            comingsoonPanel.GetComponent<Animator>().SetBool("isClosed", true);
+            Invoke("hideComingSoonPanel", 1.2f);
+        }
+    }
+
+    void hideLevelSelectPanel()
+    {
+        levelSelectPanel.GetComponent<Animator>().SetBool("isClosed", false);
+        levelSelectPanel.SetActive(false);
+    }
+
+    void hideComingSoonPanel()
+    {
+        comingsoonPanel.GetComponent<Animator>().SetBool("isClosed", false);
+        comingsoonPanel.SetActive(false);
+    }
+}
